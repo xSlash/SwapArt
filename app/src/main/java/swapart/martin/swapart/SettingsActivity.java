24a25bae855bef1ba703dded0590e185b8183584
Martin Kristensen <slash.development@gmail.com>
2015-04-13 22:59:05 +0200

    Update app function added

--- app/src/main/java/swapart/martin/swapart/SettingsActivity.java
+++ app/src/main/java/swapart/martin/swapart/SettingsActivity.java
@@ -1,10 +1,13 @@
 package swapart.martin.swapart;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import swapart.martin.swapartmockup.R;
 
@@ -15,6 +18,16 @@ public class SettingsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        findViewById(R.id.checkForUpdates).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(GalleryActivity.this, SettingsActivity.class));
+                Uri uri = Uri.parse("https://dl.dropboxusercontent.com/u/12052609/SwapArt.apk");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
     }
 
 
