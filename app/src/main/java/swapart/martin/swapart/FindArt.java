e3ef4e7d08182703183b04c40862736a06ab8473
Martin Kristensen <slash.development@gmail.com>
2015-04-14 23:55:42 +0200

    Added swipe-able elements

--- app/src/main/java/swapart/martin/swapart/FindArt.java
+++ app/src/main/java/swapart/martin/swapart/FindArt.java
@@ -1,20 +1,105 @@
 package swapart.martin.swapart;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import swapart.martin.swapartmockup.R;
 
 
 public class FindArt extends Activity {
 
+    private ArrayList<String> al;
+    private ArrayAdapter<String> arrayAdapter;
+    private int i = 9;
+
+    @InjectView(R.id.frame) SwipeFlingAdapterView flingContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_find_art);
+        setContentView(R.layout.activity_find_art2);
+        //activity_find_art was the original
+        ButterKnife.inject(this);
+
+
+        al = new ArrayList<>();
+        al.add("picture 1");
+        al.add("picture 2");
+        al.add("picture 3");
+        al.add("picture 4");
+        al.add("picture 5");
+        al.add("picture 6");
+        al.add("picture 7");
+        al.add("picture 8");
+
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.swipeart_element, R.id.helloText, al );
+
+
+        flingContainer.setAdapter(arrayAdapter);
+        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+            @Override
+            public void removeFirstObjectInAdapter() {
+                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                Log.d("LIST", "removed object!");
+                al.remove(0);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onLeftCardExit(Object dataObject) {
+                //Do something on the left!
+                //You also have access to the original object.
+                //If you want to use it just cast it (String) dataObject
+                //Toast.makeText(FindArt.this, "Left!", Toast.LENGTH_LONG).show();
+                makeToast(FindArt.this, "Left!");
+            }
+
+            @Override
+            public void onRightCardExit(Object dataObject) {
+                makeToast(FindArt.this, "Right!");
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int itemsInAdapter) {
+                // Ask for more data here
+                al.add("picture ".concat(String.valueOf(i)));
+                arrayAdapter.notifyDataSetChanged();
+                Log.d("LIST", "notified");
+                i++;
+            }
+
+            @Override
+            public void onScroll(float scrollProgressPercent) {
+                View view = flingContainer.getSelectedView();
+                view.findViewById(R.id.item_swipe_right_indicator).setAlpha(scrollProgressPercent < 0 ? -scrollProgressPercent : 0);
+                view.findViewById(R.id.item_swipe_left_indicator).setAlpha(scrollProgressPercent > 0 ? scrollProgressPercent : 0);
+            }
+        });
+
+
+        // Optionally add an OnItemClickListener
+        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int itemPosition, Object dataObject) {
+
+                makeToast(FindArt.this, "Clicked!");
+            }
+        });
     }
 
 
@@ -38,5 +123,22 @@ public class FindArt extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    static void makeToast(Context ctx, String s){
+        Toast.makeText(ctx, s, Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.right)
+    public void right() {
+        /**
+         * Trigger the right event manually.
+         */
+        flingContainer.getTopCardListener().selectRight();
+    }
+
+    @OnClick(R.id.left)
+    public void left() {
+        flingContainer.getTopCardListener().selectLeft();
     }
 }
