cfb1c9cb3ea757d05f805650da76176c1e99f19c
Martin Kristensen <slash.development@gmail.com>
2015-05-19 15:20:40 +0200

    Login layout changes

--- app/src/main/java/swapart/martin/swapart/LogInActivity.java
+++ app/src/main/java/swapart/martin/swapart/LogInActivity.java
@@ -1,5 +1,6 @@
 package swapart.martin.swapart;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -14,7 +15,7 @@ import android.widget.EditText;
 import swapart.martin.swapartmockup.R;
 
 
-public class LogInActivity extends ActionBarActivity {
+public class LogInActivity extends Activity {
 
     Context context;
 
